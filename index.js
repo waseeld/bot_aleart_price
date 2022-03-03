250c6e58a430349bbff8b41d57ee368f2d1ff835
waseeld <dddwas12@gmail.com>
2022-03-03 20:03:18 +0300

    the begnanin of command /newAlert

--- index.js
+++ index.js
@@ -19,4 +19,18 @@ bot.command('price', (ctx) => {
     })
 })
 
+bot.command('newAlert', (ctx) => {
+    let arg1 = ctx.message.text.split(" ")[1]
+    let msg = undefined
+    if(isNaN(arg1)){
+        msg = `Make sure you send somthing like that :
+        /newAlert 300
+        `
+    }else{
+        msg = `Done, we make Alert for you on ${arg1}`
+    }
+
+    ctx.reply(msg, {reply_to_message_id: ctx.message.message_id})
+})
+
 bot.start()
